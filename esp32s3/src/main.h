06d4a0bcf163bd3b6d22b35046f25d9a13ae3d09
Hajime Fujimoto <hajime2008x@yahoo.co.jp>
2022-09-15 22:23:56 +0900

    First Commit

---
+++ esp32s3/src/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+
+extern WebsocketsServer wsServer;
+void _setup(const char* ver);
+void _loop(void);
+void esp32s3_tone(uint8_t port, int16_t freq, int16_t ms);
+int esp32s3_analogRead(uint8_t port, uint16_t count);
+void showLED(int r, int g, int h);
+#endif  // main_h
